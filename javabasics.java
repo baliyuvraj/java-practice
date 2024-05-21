fd2d0a5e23980c24cf6c07601ed31d8504f55f79
baliyuvraj <baliyuvraj439@gmail.com>
2024-05-22 04:18:20 +0530

    first java program

---
+++ javabasics.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class javabasics{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter first number : ");
+        int a = sc.nextInt();
+        System.out.print("enter second number number : ");
+        int b = sc.nextInt();
+        
+        int c = a+b ;
+        System.out.print("the result is : " + c);
+    }
+}
