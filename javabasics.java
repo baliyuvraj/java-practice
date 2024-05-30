e8d456afea6940aa46c7d973255dd5dd9b4282f8
baliyuvraj <baliyuvraj439@gmail.com>
2024-05-30 08:18:18 +0530

    program to convert degree celcius to fahrenheit

--- javabasics.java
+++ javabasics.java
@@ -1,13 +1,10 @@
 import java.util.*;
 public class javabasics{
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("enter first number : ");
-        int a = sc.nextInt();
-        System.out.print("enter second number number : ");
-        int b = sc.nextInt();
-        
-        int c = a+b ;
-        System.out.print("the result is : " + c);
+    Scanner sc = new Scanner(System.in);
+    System.out.print("please enter temperature in celcius : ");
+    float tempC=sc.nextFloat();
+    float tempF = (tempC * 9/5) + 32;
+    System.out.print("the temperature in farenhite is " + tempF);
     }
 }
