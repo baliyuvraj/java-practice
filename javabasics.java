2bfed4e760d6d02b0ac0dc2b44c9f42ab7742a27
baliyuvraj <baliyuvraj439@gmail.com>
2024-07-14 01:33:26 +0530

    functions 1st program

--- javabasics.java
+++ javabasics.java
@@ -1,10 +1,16 @@
 import java.util.*;
 public class javabasics{
     public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    System.out.print("please enter temperature in celcius : ");
-    float tempC=sc.nextFloat();
-    float tempF = (tempC * 9/5) + 32;
-    System.out.print("the temperature in farenhite is " + tempF);
+        int ans = sum2();
+        System.out.print(ans);
+    }
+    static int sum2(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter num1 : ");
+        int num1 = sc.nextInt();
+        System.out.print("enter num2 : ");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2 ;
+        return sum ;
     }
 }
